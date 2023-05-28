2056c9c34c77487ae92b7fdfb0681b0d3439fbb7
cristiancfe <cristiancfe@gmail.com>
2023-05-28 16:38:40 -0300

    array e marquee html

---
+++ array.js
@@ -0,0 +1,11 @@
+let array = ['cristian', 'sandra', 'Miguel', 'Sibele']
+
+//mostras os elemtos do array
+for(let elemento of array){
+  console.log(elemento);
+}
+
+//mostra as posições dos elementos
+for(let elemento in array){
+  console.log(elemento);
+}
